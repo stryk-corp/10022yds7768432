565c45f15acaa79731f190c7793d003b7faa2e48
Dr Stryk <tobennaike21@gmail.com>
2025-11-04 21:52:43 +0000

    correct it

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import './globals.css';
 import { cn } from '@/lib/utils';
 
 export const metadata: Metadata = {
-  title: 'FCT Youth Connect',
+  title: 'FCTASDS Youth Connect',
   description: 'Empowering Tomorrow\'s Leaders Today in the Federal Capital Territory',
 };
 
