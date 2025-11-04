0b95faf75275da8e661bce02842cb3de50596373
Dr Stryk <tobennaike21@gmail.com>
2025-11-04 22:25:53 +0000

    The Fct minister is https://raw.githubusercontent.com/stryk-corp/img-rep

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -24,7 +24,7 @@ export const stats = [
 ];
 
 export const leadership = [
-  { name: 'FCT Minister', title: '—', imageId: 'leadership_6' },
+  { name: 'Ezenwo Nyesom Wike', title: 'FCT Minister', imageId: 'leadership_6' },
   { name: 'FCT Minister of State', title: '—', imageId: 'leadership_7' },
   { name: 'Hon. Ango Abdullahi Suleiman', title: 'Mandate Secretary', imageId: 'leadership_1' },
   { name: 'Mrs. Nathan Nancy', title: 'Permanent Secretary', imageId: 'leadership_2' },
